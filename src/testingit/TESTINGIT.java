4bfd0b0e081eb6fb7dfe969840671e1506cfc54d
nikos <nikos.karapas86@gmail.com>
2019-06-24 19:16:04 +0300

    just a message

---
+++ src/testingit/TESTINGIT.java
@@ -0,0 +1,15 @@
+
+package testingit;
+
+
+public class TESTINGIT {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+        
+        
+        
+    }
+    
+}
